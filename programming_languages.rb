9a96a2b66543e4a66e7e3b9b83b6d9d8d87c0ce6
plai217 <philiplai217@gmail.com>
2015-06-09 12:47:49 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
   # your code here
+  a = languages.invert
+  a.each do |b,c|
+    b.each do |d,e|
+    e[:style] = [c]
+    end
+  end
+  
+  a = a.keys
+  final_hash = a[0].merge(a[1])
+
+  final_hash[:javascript][:style] << :oo ## couldn't figure this part out, will revisit.
+  return final_hash
+
 end
+
