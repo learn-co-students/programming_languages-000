4f227159e90740dfba3d5482e596a75ad0b8425a
aeremin <aeremin@maxus.ru>
2015-06-16 12:21:06 +0300

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
 def reformat_languages(languages)
   # your code here
+  languages.each_with_object({}) do |(style, style_value), result|
+	style_value.each do |k,v|
+	  if !result.has_key?(k) then
+	    result.store(k, { :style => [style], :type => v[:type] })
+	  else
+        result[k][:style] << style
+      end	  
+	end
+  end
 end
