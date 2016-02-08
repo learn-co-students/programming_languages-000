fae8e2187dd7ba57f2ed3641bf10f3f2bd0bdbe8
Patti O'Neill <pdoneill26@gmail.com>
2016-02-08 22:25:29 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,12 +1,54 @@
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
 
+  }
+}
 
 def reformat_languages(languages)
    a = Hash.new
-  languages.each do | style, language |
-     language.each do | key, value|
-       a[key]= { key => value }
-       value[:style] = style
-     end
+  languages[:oo].each do | language, type |
+    a[language]= type
    end
+  languages[:functional].each do | language, type |
+    a[language]= type
+  end
+  a.each do | key, value |
+    a[key][:style]= []
+  end
+  a.each_key do | key |
+    if languages[:oo].key?(key) == true && languages[:functional].key?(key) == true
+      a[key][:style]= [:oo, :functional]
+    elsif languages[:oo].key?(key) == true
+      a[key][:style]= [:oo]
+    else languages[:functional].key?(key) == true
+      a[key][:style]= [:functional]
+    end
  end
+end
 
