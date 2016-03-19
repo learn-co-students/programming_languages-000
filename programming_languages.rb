42b45899bbb29bd094a69ac5a4c0e576b3ef221c
mendyismyname <m.seldowitz@gmail.com>
2016-03-19 16:25:42 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  formated = {}
+  languages.each do |style, langs_hash|
+    langs_hash.each do |lang, types_hash|
+      formated[lang] ||= {}
+      types_hash.each do |label, type|
+        formated[lang][label] ||= type
+        formated[lang][:style] ||= []
+        formated[lang][:style] << style
+      end
+    end
+  end
+formated
 end
