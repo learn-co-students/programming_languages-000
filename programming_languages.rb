fd0bc4b77521cbf356ba456040dc86600a6f0de7
Alex Katalkin <alexey.katalkin@gmail.com>
2017-09-19 02:05:51 +0000

    proglang

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, language_info|
+
+    language_info.each do |language, type|
+
+      same_hash = new_hash.select { |key, hash| key == language }
+
+        same_hash.find do |x,y|
+          y[:style].push(style)
+        end
+
+        if same_hash == {}
+          new_hash[language] = {type: type.values.join}.merge!(:style => [style])
+        end
+
+    end
+
+  end
+  new_hash
 end
+
+#reformat_languages(languages)
